92efc1ad44dda401afcf359bea92b3f4e2a161e8
mellyssamnds <mellyssa.smendes@gmail.com>
2026-08-03 21:24:37 -0300

    build: inicializa projeto Spring Boot com Java 21

---
+++ pix-management-api/src/main/java/com/mellyssamnds/pix_management_api/PixManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.mellyssamnds.pix_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixManagementApiApplication.class, args);
+	}
+
+}
